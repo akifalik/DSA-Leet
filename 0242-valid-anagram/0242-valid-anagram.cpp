d40a8aa4d90a07f3ba5ecf1bf3b69457c24d03f5
akif ali khan <khanakifali495@gmail.com>
2026-08-09 18:24:09 +0530

    Time: 3 ms (45.46%), Space: 9.8 MB (49.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
-            return false;
+        if(s.length()!=t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(), t.end());
 
-        vector<int> freq(26, 0);
 
-        for (char c : s)
-            freq[c - 'a']++;
-
-        for (char c : t)
-            freq[c - 'a']--;
-
-        for (int count : freq) {
-            if (count != 0)
-                return false;
-        }
-
-        return true;
+        return s==t;
     }
 };
